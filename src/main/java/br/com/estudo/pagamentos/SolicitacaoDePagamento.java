03af79ca23ae5540a0865433fa88981eba77b0ef
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-22 14:16:39 -0300

    feat: Representando uma solicitação de pagamento.

---
+++ src/main/java/br/com/estudo/pagamentos/SolicitacaoDePagamento.java
@@ -0,0 +1,41 @@
+package br.com.estudo.pagamentos;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class SolicitacaoDePagamento {
+    private String numeroDoPedido;
+    private String tipoDoDocumento;
+    private String numeroDoDocumento;
+    private LocalDate dataDeEmissaoDoDocumento;
+    private String formaDePagamento;
+    private String tipoDeMoeda;
+    private BigDecimal valorTotal;
+    private LocalDateTime dataDeRegistro;
+    private LocalDateTime dataUltimaAtualizacao;
+    private String status;
+    private String tipoDeSolicitacao;
+    private String observacoesContabilidade;
+    private String observacao;
+    private String motivo;
+
+    public SolicitacaoDePagamento(String numeroDoPedido, String tipoDoDocumento, String numeroDoDocumento, LocalDate dataDeEmissaoDoDocumento, String formaDePagamento, String tipoDeMoeda, BigDecimal valorTotal) {
+        if (numeroDoPedido == "" || numeroDoPedido == null
+                || tipoDoDocumento == "" || tipoDoDocumento == null
+                || numeroDoDocumento == "" || numeroDoDocumento == null
+                || dataDeEmissaoDoDocumento == null
+                || formaDePagamento == "" || formaDePagamento == null
+                || tipoDeMoeda == "" || tipoDeMoeda == null
+                || valorTotal.intValue() == 0 || valorTotal == null
+        ) throw new IllegalArgumentException("Dados obrigatórios não podem ser vazios ou nulos.");
+
+        this.numeroDoPedido = numeroDoPedido;
+        this.tipoDoDocumento = tipoDoDocumento;
+        this.numeroDoDocumento = numeroDoDocumento;
+        this.dataDeEmissaoDoDocumento = dataDeEmissaoDoDocumento;
+        this.formaDePagamento = formaDePagamento;
+        this.tipoDeMoeda = tipoDeMoeda;
+        this.valorTotal = valorTotal;
+    }
+}
---
+++ src/test/java/br/com/estudo/pagamentos/TestaSolicitacaoDePagamento.java
@@ -0,0 +1,27 @@
+package br.com.estudo.pagamentos;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class TestaSolicitacaoDePagamento {
+    public static void main(String[] args) {
+        TestaSolicitacaoDePagamento solicitacao = new TestaSolicitacaoDePagamento();
+//        solicitacao.naoDeveCriarSolicitacaoDePagamentoAoPassarArgumentosNulosOuVazios();
+//        solicitacao.naoDeveCriarSolicitacaoDePagamentoAoPassarTotalZero();
+        solicitacao.deveCriarSolicitacaoDePagamento();
+    }
+
+    public void naoDeveCriarSolicitacaoDePagamentoAoPassarArgumentosNulosOuVazios() {
+        SolicitacaoDePagamento solicitacao1 = new SolicitacaoDePagamento("", "", "", null, "", "", new BigDecimal(0));
+        SolicitacaoDePagamento solicitacao2 = new SolicitacaoDePagamento(null, null, null, null, null, null, null);
+    }
+
+    public void naoDeveCriarSolicitacaoDePagamentoAoPassarTotalZero() {
+        new SolicitacaoDePagamento("123", "456", "789", LocalDate.now(), "BOLETO", "BRL", new BigDecimal(0));
+    }
+
+    public void deveCriarSolicitacaoDePagamento() {
+        new SolicitacaoDePagamento("123", "456", "789", LocalDate.now(), "BOLETO", "BRL", new BigDecimal(10));
+    }
+
+}
