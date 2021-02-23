ee39b1acfb22e1a238710e256ea9b7ddf1c7bd50
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-23 10:12:18 -0300

    feat: Instalando dependência para automatizar os testes.

--- src/test/java/br/com/estudo/pagamentos/TestaSolicitacaoDePagamento.java
+++ src/test/java/br/com/estudo/pagamentos/TestaSolicitacaoDePagamento.java
@@ -1,25 +1,24 @@
 package br.com.estudo.pagamentos;
 
+import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class TestaSolicitacaoDePagamento {
-    public static void main(String[] args) {
-        TestaSolicitacaoDePagamento solicitacao = new TestaSolicitacaoDePagamento();
-//        solicitacao.naoDeveCriarSolicitacaoDePagamentoAoPassarArgumentosNulosOuVazios();
-//        solicitacao.naoDeveCriarSolicitacaoDePagamentoAoPassarTotalZero();
-        solicitacao.deveCriarSolicitacaoDePagamento();
-    }
 
+    @Test
     public void naoDeveCriarSolicitacaoDePagamentoAoPassarArgumentosNulosOuVazios() {
         SolicitacaoDePagamento solicitacao1 = new SolicitacaoDePagamento(null, "", null, null, "", "", new BigDecimal(0));
         SolicitacaoDePagamento solicitacao2 = new SolicitacaoDePagamento(null, null, null, null, null, null, null);
     }
 
+    @Test
     public void naoDeveCriarSolicitacaoDePagamentoAoPassarTotalZero() {
         new SolicitacaoDePagamento(123, "456", 789, LocalDate.now(), "BOLETO", "BRL", new BigDecimal(0));
     }
 
+    @Test
     public void deveCriarSolicitacaoDePagamento() {
         new SolicitacaoDePagamento(123, "456", 789, LocalDate.now(), "BOLETO", "BRL", new BigDecimal(10));
     }
