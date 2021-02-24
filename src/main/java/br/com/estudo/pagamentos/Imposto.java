abc48276aea09ebc3140685eea482ae49c2a439a
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-23 23:45:53 -0300

    feat: Casos de teste para gerar imposto.

--- src/main/java/br/com/estudo/pagamentos/Imposto.java
+++ src/main/java/br/com/estudo/pagamentos/Imposto.java
@@ -15,6 +15,10 @@ public class Imposto {
         this.valor = valor;
     }
 
+    public TipoDeImposto getTipo() {
+        return tipo;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
