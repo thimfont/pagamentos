62521e2f761a97dff8f350602005a661dab1164b
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-23 23:26:32 -0300

    refactor: Extraindo linha repetida para método de setup ao rodar o teste.

--- src/test/java/br/com/estudo/pagamentos/TestaPagamentoFornecedor.java
+++ src/test/java/br/com/estudo/pagamentos/TestaPagamentoFornecedor.java
@@ -1,16 +1,22 @@
 package br.com.estudo.pagamentos;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class TestaPagamentoFornecedor {
+    private SolicitacaoDePagamentoParaFornecedor boleto;
+
+    @BeforeEach
+    public void setupAntesDeCadaTeste() {
+        boleto = FabricaSolicitacaoDePagamento.paraFornecedor();
+    }
 
     @Test
     public void naoDeveriaCadastrarAhMesmaParcelaDuasVezes() {
-        SolicitacaoDePagamentoParaFornecedor boleto = FabricaSolicitacaoDePagamento.paraFornecedor();
         boleto.adicionaParcela(1, LocalDate.now(), new BigDecimal(10));
         boleto.adicionaParcela(1, LocalDate.now(), new BigDecimal(10));
         boleto.adicionaParcela(2, LocalDate.now(), new BigDecimal(20));
@@ -19,7 +25,6 @@ public class TestaPagamentoFornecedor {
 
     @Test
     public void seVencimentoParaPagamentoEhMenorQue3DiasDeveriaSerTratadoComoUrgente() {
-        SolicitacaoDePagamentoParaFornecedor boleto = FabricaSolicitacaoDePagamento.paraFornecedor();
         LocalDate vencimento = LocalDate.now().plusDays(2);
         boleto.adicionaParcela(1, vencimento, new BigDecimal("200"));
         Assertions.assertEquals(true, boleto.ehUrgente());
@@ -27,7 +32,6 @@ public class TestaPagamentoFornecedor {
 
     @Test
     public void seVencimentoParaPagamentoEhMaiorOuIgualAh3DiasNaoEhUrgente() {
-        SolicitacaoDePagamentoParaFornecedor boleto = FabricaSolicitacaoDePagamento.paraFornecedor();
         LocalDate vencimento = LocalDate.now().plusDays(3);
         boleto.adicionaParcela(1, vencimento, new BigDecimal("200"));
         Assertions.assertEquals(false, boleto.ehUrgente());
