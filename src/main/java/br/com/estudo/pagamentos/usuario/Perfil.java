2792751dadf7aea421e032104637988809d791a3
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-24 23:45:23 -0300

    feat: Criando os papeis/funções que o usuário poderá ter no sistema.

--- src/main/java/br/com/estudo/pagamentos/usuario/Perfil.java
+++ src/main/java/br/com/estudo/pagamentos/usuario/Perfil.java
@@ -1,9 +1,19 @@
 package br.com.estudo.pagamentos.usuario;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Perfil {
     private TipoDePerfil nome;
-    
+    private Set<Papel> papeis;
+
     public Perfil(TipoDePerfil nome) {
         this.nome = nome;
+        this.papeis = new HashSet<>();
+    }
+
+    public void adicionaPapel(Papel funcao) {
+        if (funcao == null) throw new IllegalArgumentException("Função do perfil do usuário não pode ser nula.");
+        this.papeis.add(funcao);
     }
 }
