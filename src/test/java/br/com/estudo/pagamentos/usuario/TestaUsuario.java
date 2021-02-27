015510b61df437315ac105a3fc578f4e6811bcd5
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-27 00:09:57 -0300

    refactor: Ajustando testes para criar usuário recebendo tipo de perfil.

--- src/test/java/br/com/estudo/pagamentos/usuario/TestaUsuario.java
+++ src/test/java/br/com/estudo/pagamentos/usuario/TestaUsuario.java
@@ -13,57 +13,48 @@ public class TestaUsuario {
     @Test
     public void naoDeveriaCriarUsuarioComEmailInvalido() {
         String semArroba = "teste";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", semArroba, new Perfil(TipoDePerfil.USUARIO)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", semArroba, TipoDePerfil.USUARIO));
 
         String possuiHifemNoComecoDoDominio = "teste@-teste.com";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", possuiHifemNoComecoDoDominio, new Perfil(TipoDePerfil.USUARIO)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", possuiHifemNoComecoDoDominio, TipoDePerfil.USUARIO));
 
         String possuiHifemNoFimDoDominio = "teste@teste.com-";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", possuiHifemNoFimDoDominio, new Perfil(TipoDePerfil.USUARIO)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", possuiHifemNoFimDoDominio, TipoDePerfil.USUARIO));
 
         String semPontoNoDominio = "teste@testecom";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", semPontoNoDominio, new Perfil(TipoDePerfil.USUARIO)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", semPontoNoDominio, TipoDePerfil.USUARIO));
 
         String doisArrobas = "teste@exemplo@teste.com";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", doisArrobas, new Perfil(TipoDePerfil.USUARIO)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Usuario("Teste", doisArrobas, TipoDePerfil.USUARIO));
     }
 
     @Test
     public void deveriaCriarUsuarioComEmailValido() {
         String email = "teste@teste.com";
-        Usuario usuario1 = new Usuario("Teste", email, new Perfil(TipoDePerfil.USUARIO));
+        Usuario usuario1 = new Usuario("Teste", email, TipoDePerfil.USUARIO);
         Assertions.assertEquals(email, usuario1.getEmail());
 
         String emailComHifemAntesDoArroba = "teste-teste@teste.com";
-        Usuario usuario2 = new Usuario("Teste", emailComHifemAntesDoArroba, new Perfil(TipoDePerfil.USUARIO));
+        Usuario usuario2 = new Usuario("Teste", emailComHifemAntesDoArroba, TipoDePerfil.USUARIO);
         Assertions.assertEquals(emailComHifemAntesDoArroba, usuario2.getEmail());
 
         String emailComHifeNoDominio = "teste@teste-exemplo.com";
-        Usuario usuario3 = new Usuario("Teste", emailComHifeNoDominio, new Perfil(TipoDePerfil.USUARIO));
+        Usuario usuario3 = new Usuario("Teste", emailComHifeNoDominio, TipoDePerfil.USUARIO);
         Assertions.assertEquals(emailComHifeNoDominio, usuario3.getEmail());
     }
 
     @Test
     public void naoDeveriaCadastrarDoisPerfisIguaisParaOUsuario() {
-        Perfil perfil = new Perfil(TipoDePerfil.USUARIO);
-        Usuario usuario = new Usuario("Teste", "teste@teste.com", perfil);
-        usuario.adicionaPerfil(perfil);
-
-        Perfil perfilIgual = new Perfil(TipoDePerfil.USUARIO);
-        usuario.adicionaPerfil(perfilIgual);
-
+        Usuario usuario = new Usuario("Teste", "teste@teste.com", TipoDePerfil.USUARIO);
+        usuario.adicionaPerfil(TipoDePerfil.USUARIO);
         Assertions.assertEquals(1, usuario.getPerfis().size());
     }
 
     @Test
     public void deveriaCadastrarDiversosPerfisDiferentesParaOUsuario() {
-        Perfil perfilUsuario = new Perfil(TipoDePerfil.USUARIO);
-        Usuario usuario = new Usuario("Teste", "teste@teste.com", perfilUsuario);
-        usuario.adicionaPerfil(new Perfil(TipoDePerfil.GERENTE));
-
-        Perfil perfilFinanceiro = new Perfil(TipoDePerfil.FINANCEIRO);
-        usuario.adicionaPerfil(perfilFinanceiro);
-
+        Usuario usuario = new Usuario("Teste", "teste@teste.com", TipoDePerfil.USUARIO);
+        usuario.adicionaPerfil(TipoDePerfil.GERENTE);
+        usuario.adicionaPerfil(TipoDePerfil.FINANCEIRO);
         Assertions.assertEquals(3, usuario.getPerfis().size());
     }
 }
