d832f35b69b2567e13c0487a2d80bb4104ad4f24
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-27 21:07:49 -0300

    feat: Classe para representar fornecedor.

---
+++ src/main/java/br/com/estudo/pagamentos/fornecedor/Fornecedor.java
@@ -0,0 +1,28 @@
+package br.com.estudo.pagamentos.fornecedor;
+
+public class Fornecedor {
+    private String nome;
+    private String inscricaoEstatual;
+    private String cnpj;
+    private String cpf;
+    private String nomeDoPais;
+    private String siglaDoPais;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String codigoDoBanco;
+    private String nomeDoBanco;
+    private String agenciaDoBanco;
+    private String contaDoBanco;
+    private boolean contaDoBancoEstaAtiva;
+    private String status;
+
+    public Fornecedor(String nome, String cnpj, String cpf) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.cpf = cpf;
+    }
+}
