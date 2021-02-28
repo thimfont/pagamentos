7179e58619b3a80fddd54728793a3db2d2435efd
Thiago Melo Fontana <thimfont@gmail.com>
2021-02-27 21:49:19 -0300

    fix: Lógica que valida cep no endereço.

--- src/main/java/br/com/estudo/pagamentos/fornecedor/Endereco.java
+++ src/main/java/br/com/estudo/pagamentos/fornecedor/Endereco.java
@@ -12,9 +12,13 @@ public class Endereco {
 
     public Endereco(String cep) {
         if (cep == null || cep == "") throw new IllegalArgumentException("CEP não pode ser nulo ou vazio");
-        if (!cep.matches("[0-9]{2}\\.?[0-9]{3}\\-?[0-9]{2}"))
-            throw new IllegalArgumentException("CEP não pode ser nulo ou vazio");
+        if (!cep.matches("[0-9]{5}\\-?[0-9]{3}"))
+            throw new IllegalArgumentException("CEP invlálido");
 
         this.cep = cep;
+    }
+
+    public String getCep() {
+        return cep;
     }
 }
