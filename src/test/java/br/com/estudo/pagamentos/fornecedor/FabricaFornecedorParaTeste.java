291bab8e3dee360f7d6b17bdc7e80baa0e5477db
Thiago Melo Fontana <thimfont@gmail.com>
2021-03-01 10:48:19 -0300

    feat: Novo método para criar fornecedor físico para teste.

--- src/test/java/br/com/estudo/pagamentos/fornecedor/FabricaFornecedorParaTeste.java
+++ src/test/java/br/com/estudo/pagamentos/fornecedor/FabricaFornecedorParaTeste.java
@@ -5,4 +5,8 @@ public class FabricaFornecedorParaTeste {
     public static Fornecedor tipoJuridico() {
         return new Fornecedor(new CNPJ("91375860000165"), TipoDeFornecedor.FORNECEDOR_JURIDICO);
     }
+
+    public static Fornecedor tipoFisico() {
+        return new Fornecedor(new CNPJ("78945612358"), TipoDeFornecedor.FORNECEDOR_JURIDICO);
+    }
 }
