643b523b8d88cb5c5b403bc749a6ad3858c14b56
Thiago Melo Fontana <thimfont@gmail.com>
2021-03-03 12:58:00 -0300

    fix: Defindo tipo de fornecedor correto no teste.

--- src/test/java/br/com/estudo/pagamentos/fornecedor/FabricaFornecedorParaTeste.java
+++ src/test/java/br/com/estudo/pagamentos/fornecedor/FabricaFornecedorParaTeste.java
@@ -7,6 +7,6 @@ public class FabricaFornecedorParaTeste {
     }
 
     public static Fornecedor tipoFisico() {
-        return new Fornecedor(new CPF("78945612358"), TipoDeFornecedor.FORNECEDOR_JURIDICO);
+        return new Fornecedor(new CPF("78945612358"), TipoDeFornecedor.FORNECEDOR_FISICO);
     }
 }
