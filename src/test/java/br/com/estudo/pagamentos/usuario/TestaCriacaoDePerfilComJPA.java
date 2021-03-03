4fb2dba1c1e56b527ff00c45f9ab2c8df693fbd4
Thiago Melo Fontana <thimfont@gmail.com>
2021-03-02 21:16:21 -0300

    feat: Adicionando um TODO para realizar no futuro evitando duplicação no cadastro do perfil.

--- src/test/java/br/com/estudo/pagamentos/usuario/TestaCriacaoDePerfilComJPA.java
+++ src/test/java/br/com/estudo/pagamentos/usuario/TestaCriacaoDePerfilComJPA.java
@@ -5,6 +5,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class TestaCriacaoDePerfilComJPA {
+    // TODO: como evitar duplicar o perfil no banco?
+    /* atualmente, ao adicionar um novo usuário, mesmo que ele possua um peril já cadastrado no banco de dados,
+    como por exemplo ADMNISTRADOR, ele será recastrado duplicando o mesmo perfil.
+    * */
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pagamentos");
         EntityManager em = emf.createEntityManager();
